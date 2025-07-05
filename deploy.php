d68d59ad35808029dec290ca5733d94a0b289e7c
CherryRadiator <yuriyfursa911@gmail.com>
2025-07-05 04:08:47 +0300

    no

--- deploy.php
+++ deploy.php
@@ -1,6 +1,6 @@
 <?php
 // Простая загрузка .env файла
-$env_file = '/.env';
+$env_file = './.env';
 if (file_exists($env_file)) {
     $lines = file($env_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
